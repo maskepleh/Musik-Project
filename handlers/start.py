3ad7257bc2fa84d51ab715bd95335e3732ce87ee
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-29 00:28:31 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -61,10 +61,10 @@ async def start_(client: Client, message: Message):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🌐 Wiki's Page", url="https://github.com/levina-lab/veezmusic/wiki/Veez-Music-Wiki's")
+                        "🌐 Wiki's Page", url="https://github.com/Askarbot/SkyzuMusic/wiki/Skyzu-Music-Wiki's")
                 ],[
                     InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/levina-lab/VeezMusic"
+                        "🧪 Source Code 🧪", url="https://github.com/Askarbot/SkyzuMusic"
                     )
                 ]
             ]
